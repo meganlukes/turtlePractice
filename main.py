420093e018a6c1d0f7589dc8e7778c6f97af36e6
Megan Lukes@gmail.com <meglukes@gmail.com>
2022-01-12 11:04:12 -0500

    added square

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 from turtle import Turtle, Screen
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("DeepPink")
-# timmy.forward(100)
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+# can use from turtle import * to import all modules, not often used because it is confusing
+# can also "import [module] as [alias]", i.e. "import turtle as t" so you can write "spike = t.Turtle()" instead of "spike = turtle.Turtle()"
 
-# from prettytable import PrettyTable
-# table = PrettyTable()
-#
-# table.add_column("Common Name", ["Black Bamboo", "Angel Mist Bamboo", "Blue Chungii"])
-# table.add_column("Scientific Name", ["Phyllostachys nigra", "Dendrocalamus minor Amoenus", "Bambusa chungii"])
-# table.align = "r"
-# table.title = "Bamboo"
-# print(table)
 
 spike = Turtle()
 screen = Screen()
 spike.shape("arrow")
 screen.colormode(255)
 spike.color(116, 95, 217)      #Weird, RGB was supposed to be pencolor() not color()
-spike.forward(100)
+#draw a square
+for _ in range(4):
+    spike.forward(100)
+    spike.right(90)
 
 
 
@@ -52,3 +41,13 @@ screen.exitonclick()
 # print(black_bamboo.scientific_name)
 # if black_bamboo.in_stock == False:
 #     print("Out of stock")
+
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+#
+# table.add_column("Common Name", ["Black Bamboo", "Angel Mist Bamboo", "Blue Chungii"])
+# table.add_column("Scientific Name", ["Phyllostachys nigra", "Dendrocalamus minor Amoenus", "Bambusa chungii"])
+# table.align = "r"
+# table.title = "Bamboo"
+# print(table)
